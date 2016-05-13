0f7145fbc3c916917813565bd829f8f085535000
ManashRaja <mpdmanash@gmail.com>
2016-05-14 03:47:40 +0530

    Add packages to env_setup

--- env_setup.sh
+++ env_setup.sh
@@ -4,4 +4,6 @@ sudo -E apt-get update  &&
 sudo -E apt-get install screen python-wxgtk2.8 python-serial python-pyparsing python-matplotlib python-opencv python-pip python-numpy python-dev libxml2-dev libxslt-dev &&
 sudo -E pip install pymavlink &&
 sudo -E pip install mavproxy &&
-sudo -E pip install dronekit
+sudo -E pip install dronekit &&
+sudo -E pip install dronekit-sitl -UI &&
+sudo -E pip install Flask

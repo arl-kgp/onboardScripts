7512287debe49f1dce796fc4bf10fd3532b23d9b
ManashRaja <mpdmanash@gmail.com>
2016-05-17 03:33:27 +0530

    Enable input of gps waypoints in array

--- goToPointGPS.py
+++ goToPointGPS.py
@@ -1,24 +1,26 @@
 from dronekit import connect, VehicleMode, LocationGlobalRelative
 import time
 import math
-P1_LAT = -35.3613541
-P1_LON = 149.1652181
-P1_ALT = 2
-P2_LAT = -35.363244
-P2_LON = 149.168801
-P2_ALT = 2 
-#Set up option parsing to get connection string
-import argparse  
+import argparse
+
+P1 = [22.31890386, 87.30263576, 2]
+P2 = [22.31884598, 87.3024447, 2]
+
+gps_points = [P1, P2]
+
+
+# Set up option parsing to get connection string
+
 
 parser = argparse.ArgumentParser(description='Commands vehicle using vehicle.simple_goto.')
-parser.add_argument('--connect', 
-                   help="Vehicle connection target string. If not specified, SITL automatically started and used.")
+parser.add_argument('--connect',
+                    help="Vehicle connection target string. If not specified, SITL automatically started and used.")
 args = parser.parse_args()
 
 connection_string = args.connect
 sitl = None
 
-#Start SITL if no connection string specified
+# Start SITL if no connection string specified
 if not args.connect:
     print "Starting copter simulator (SITL)"
     from dronekit_sitl import SITL
@@ -39,73 +41,71 @@ def arm_and_takeoff(aTargetAltitude):
     Arms vehicle and fly to aTargetAltitude.
     """
 
-    print "Basic pre-arm checks"
+    # print "Basic pre-arm checks"
     # Don't try to arm until autopilot is ready
-    while not vehicle.is_armable:
-        print " Waiting for vehicle to initialise..."
-        time.sleep(1)
+    # while not vehicle.is_armable:
+    #    print " Waiting for vehicle to initialise..."
+    #    time.sleep(1)
 
-        
     print "Arming motors"
     # Copter should arm in GUIDED mode
     vehicle.mode = VehicleMode("GUIDED")
-    vehicle.armed = True    
+    vehicle.armed = True
 
     # Confirm vehicle armed before attempting to take off
-    while not vehicle.armed:      
+    while not vehicle.armed:
         print " Waiting for arming..."
         time.sleep(1)
 
     print "Taking off!"
-    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+    vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
 
     # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command 
     #  after Vehicle.simple_takeoff will execute immediately).
     while True:
-        print " Altitude: ", vehicle.location.global_relative_frame.alt 
-        #Break and return from function just below target altitude.        
-        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95: 
+        print " Altitude: ", vehicle.location.global_relative_frame.alt
+        # Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
             print "Reached target altitude"
             break
         time.sleep(1)
-def measure(lat1, lon1, lat2, lon2) : # generally used geo measurement function
-    R = 6378.137 #Radius of earth in KM
-    dLat = (lat2-lat1)*math.pi/180
-    dLon = (lon2-lon1)*math.pi/180
-    a = math.sin(dLat/2)*math.sin(dLat/2) + math.cos(lat1*math.pi/180)*math.cos(lat2*math.pi/180)*math.sin(dLon/2)*math.sin(dLon/2)
-    c = 2*math.atan2(math.sqrt(a), math.sqrt(1-a))
-    d = R*c
-    return d*1000 #meters
 
-arm_and_takeoff(2)
 
-print "Set default/target airspeed to 1"
-vehicle.airspeed = 1
+def measure(lat1, lon1, lat2, lon2):  # generally used geo measurement function
+    R = 6378.137  # Radius of earth in KM
+    dLat = (lat2 - lat1) * math.pi / 180
+    dLon = (lon2 - lon1) * math.pi / 180
+    a = math.sin(dLat / 2) * math.sin(dLat / 2) + math.cos(lat1 * math.pi / 180) * math.cos(lat2 * math.pi / 180) * math.sin(dLon / 2) * math.sin(dLon / 2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    d = R * c
+    return d * 1000  # meters
 
-print "Going towards first point (groundspeed set to 1 m/s) ..."
-point1 = LocationGlobalRelative(P1_LAT,P1_LON,P1_ALT)
-vehicle.simple_goto(point1,groundspeed=1)
-#printing the distance from pioint 1 until we reach point 1 with an error of 1 meter
-while measure(vehicle.location.global_relative_frame.lat,vehicle.location.global_relative_frame.lon,P1_LAT,P1_LON)>1 :
-    print measure(vehicle.location.global_relative_frame.lat,vehicle.location.global_relative_frame.lon,P1_LAT,P1_LON)
-    time.sleep(3)
-print "reached point 1"
-print "Going towards second point (groundspeed set to 1 m/s) ..."
-point2 = LocationGlobalRelative(P2_LAT, P2_LON, P2_ALT)
-vehicle.simple_goto(point2, groundspeed=1)
-#printing the distance from point 2 until we reach point 2 with an error of 1 meter
-while measure(vehicle.location.global_relative_frame.lat,vehicle.location.global_relative_frame.lon,P2_LAT,P2_LON)>1 :
-    print measure(vehicle.location.global_relative_frame.lat,vehicle.location.global_relative_frame.lon,P2_LAT,P2_LON)
-    time.sleep(3)
-print "reached point 2"
 
-print "landing"
-vehicle.mode = VehicleMode("LAND")
+def main(): 
+    arm_and_takeoff(2)
 
-#Close vehicle object before exiting script
-print "Close vehicle object"
-vehicle.close()
+    print "Set default/target airspeed to 1"
+    vehicle.airspeed = 1
 
-# Shut down simulator if it was started.
-if sitl is not None:
-    sitl.stop()
+    for gps_point in gps_points:
+        print "Going towards next point (groundspeed set to 1 m/s) ..."
+        point1 = LocationGlobalRelative(gps_point[0], gps_point[1], gps_point[2])
+        vehicle.simple_goto(point1, groundspeed=1)
+        # printing the distance to go in meters
+        while measure(vehicle.location.global_relative_frame.lat, vehicle.location.global_relative_frame.lon, gps_point[0], gps_point[1])>1 :
+            print measure(vehicle.location.global_relative_frame.lat, vehicle.location.global_relative_frame.lon, gps_point[0], gps_point[1])
+            time.sleep(3)
+        print "reached point"
+
+    print "landing"
+    vehicle.mode = VehicleMode("LAND")
+
+    # Close vehicle object before exiting script
+    print "Close vehicle object"
+    vehicle.close()
+
+    # Shut down simulator if it was started.
+    if sitl is not None:
+        sitl.stop()
+
+main()
